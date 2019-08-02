9fb470dc34da281a7e8389137644080638accea3
larissa <larissaleonso51@gmail.com>
2019-08-02 15:39:13 -0300

    adicionar backgroundColor

---
+++ script.js
@@ -0,0 +1,42 @@
+let lista = document.querySelector("ol");
+let corpo = document.querySelector("body");
+
+let raizes = [
+    {nome:"batata"},
+    {nome:"mandioca"},
+    {nome:"rabanete"}
+];
+
+let imprimirComidas = () => { 
+    for(let raiz of raizes){
+        lista.innerHTML += `<li>${raiz.nome}</li>`
+    }   
+    
+    corpo.style.backgroundColor = Cor;
+}
+
+let adicionarComida = (callback) =>{
+    let escolheCor = prompt("Digite uma cor em ingles");
+    setTimeout(() => {
+        raizes.push({nome:"gengibre"});
+        callback();
+    }, 2000);
+};
+
+
+
+adicionarComida(imprimirComidas);
+
+
+
+
+
+
+
+// let diaSemana = "Sextou";
+
+// let callback = (resolve, reject) => {
+//     if(diaSemana == "Sextou") {
+//         resolve("Acertou");
+//     }
+// }
